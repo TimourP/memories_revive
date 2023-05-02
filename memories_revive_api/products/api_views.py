d7b3f5cf9a9695890d14e6b4c2507e7d150a67a2
timourp <petit.timour@gmail.com>
2023-05-02 13:36:13 +0200

    feat: ok for now

--- memories_revive_api/products/api_views.py
+++ memories_revive_api/products/api_views.py
@@ -4,13 +4,27 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from django.http.response import JsonResponse
 from .serializers import ProductSerializer
-from .models import Product
+from .models import Product, ProductVariant
 
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def products(request):
 	if request.method == "GET":
 		products = Product.objects.all()
+
+		product_list = []
+		for product in products:
+			variant = ProductVariant.objects.filter(product=product).first()
+			if variant is None:
+				continue
+			product_data = {
+				"name": product.name,
+				"description": product.description,
+				"main_image": f"https://edu-beerg1.odoo.com/web/image/product.product/{variant.odoo_id}/image_1024",
+				"price": variant.list_price,
+			}
+			print(product_data)
+			product_list.append(product_data)
+			break
 		
-		products_serializer = ProductSerializer(products, many=True)
-		return JsonResponse(products_serializer.data, safe=False, status=status.HTTP_200_OK)
+		return JsonResponse(product_list, safe=False, status=status.HTTP_200_OK)
